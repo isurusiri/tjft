eeae9412a0bd69a9ae5df8fc62d0d12b45d14ebb
isurusiri <isuruanatomy@gmail.com>
2020-08-28 22:56:50 +0530

    initial source

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Tweet } from "react-twitter-widgets";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Tech Jobs from Twitter.</div>
+      <div className="Jobs">
+        <div style={{ padding: 10 }}>
+          <Tweet
+            tweetId="1298377943920517121"
+            options={{ width: "800" }}
+          ></Tweet>
+        </div>
+        <div style={{ padding: 10 }}>
+          <Tweet
+            tweetId="1299006926047662080"
+            options={{ width: "800" }}
+          ></Tweet>
+        </div>
+        <div style={{ padding: 10 }}>
+          <Tweet
+            tweetId="1299001225124474880"
+            options={{ width: "800" }}
+          ></Tweet>
+        </div>
+        <div style={{ padding: 10 }}>
+          <Tweet
+            tweetId="1298998982845149186"
+            options={{ width: "800" }}
+          ></Tweet>
+        </div>
+        <div style={{ padding: 10 }}>
+          <Tweet
+            tweetId="1298992386215837703"
+            options={{ width: "800" }}
+          ></Tweet>
+        </div>
+        <div style={{ padding: 10 }}>
+          <Tweet
+            tweetId="1298956117230452739"
+            options={{ width: "800" }}
+          ></Tweet>
+        </div>
+      </div>
     </div>
   );
 }
