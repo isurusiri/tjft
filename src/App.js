d8bf33a0b244604677bb3660ed03983732940a4a
isurusiri <isuruanatomy@gmail.com>
2020-09-02 20:06:44 +0530

    removed duplicated code and aligned tweets to the top

--- src/App.js
+++ src/App.js
@@ -3,46 +3,23 @@ import "./App.css";
 import { Tweet } from "react-twitter-widgets";
 
 function App() {
+  const tweets = [
+    "1298377943920517121",
+    "1299006926047662080",
+    "1299001225124474880",
+    "1298998982845149186",
+    "1298992386215837703",
+    "1298956117230452739",
+  ];
   return (
     <div className="App">
       <div>Tech Jobs from Twitter.</div>
       <div className="Jobs">
-        <div style={{ padding: 10 }}>
-          <Tweet
-            tweetId="1298377943920517121"
-            options={{ width: "800" }}
-          ></Tweet>
-        </div>
-        <div style={{ padding: 10 }}>
-          <Tweet
-            tweetId="1299006926047662080"
-            options={{ width: "800" }}
-          ></Tweet>
-        </div>
-        <div style={{ padding: 10 }}>
-          <Tweet
-            tweetId="1299001225124474880"
-            options={{ width: "800" }}
-          ></Tweet>
-        </div>
-        <div style={{ padding: 10 }}>
-          <Tweet
-            tweetId="1298998982845149186"
-            options={{ width: "800" }}
-          ></Tweet>
-        </div>
-        <div style={{ padding: 10 }}>
-          <Tweet
-            tweetId="1298992386215837703"
-            options={{ width: "800" }}
-          ></Tweet>
-        </div>
-        <div style={{ padding: 10 }}>
-          <Tweet
-            tweetId="1298956117230452739"
-            options={{ width: "800" }}
-          ></Tweet>
-        </div>
+        {tweets.map((tweet) => (
+          <div style={{ padding: 10 }} id={tweet}>
+            <Tweet tweetId={tweet} options={{ width: "800" }}></Tweet>
+          </div>
+        ))}
       </div>
     </div>
   );
